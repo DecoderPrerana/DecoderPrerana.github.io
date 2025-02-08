a9c20771afd859d1f0b417dab718289b219be99e
Prerana <prerana.bhandari99@gmail.com>
2025-02-08 13:11:39 +0530

    resume update

--- src/data/constants.js
+++ src/data/constants.js
@@ -11,7 +11,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/PreranaDecoder",
   resume:
-    "https://drive.google.com/file/d/1Cgck-cB2NcpUKL8aPg27I5lQMrNyYxSY/view?usp=sharing",
+    "https://drive.google.com/file/d/1r75P6GTB3rmadC4-cCd-stv2z9mGLYjQ/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/preranabhandari/",
   twitter: "",
   insta: "https://www.instagram.com/capt_the_nature/",
